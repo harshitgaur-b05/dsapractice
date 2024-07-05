b21770b6cdc9c9082b0fd76a60169f307cdc5e26
harshitgaur077 <harshitgaur737@gmail.com>
2024-07-05 18:34:06 +0530

    4th  day

--- day4(array)/Longest Substring Without Repeating Characters.cpp
+++ day4(array)/Longest Substring Without Repeating Characters.cpp
@@ -2,26 +2,19 @@
 using namespace std;
 class Solution {
 public:
-    int lengthOfLongestSubstring(string str) {
-
-        if (str.size() == 0)
-            return 0;
-        int maxans = INT_MIN;
-        unordered_set<int> set;
-        int l = 0;
-        for (int r = 0; r < str.length();
-             r++) // outer loop for traversing the string
-        {
-            if (set.find(str[r]) != set.end()) // if duplicate element is found
-            {
-                while (l < r && set.find(str[r]) != set.end()) {
-                    set.erase(str[l]);
-                    l++;
-                }
-            }
-            set.insert(str[r]);
-            maxans = max(maxans, r - l + 1);
+    int lengthOfLongestSubstring(string s) {
+        if(s.size()==0)return 0;
+        vector<int> mpp(256,-1);
+        int len=0;
+        int left=0;
+        int right=0;
+        while(right<s.size()){
+            if(mpp[s[right]]!=-1) left=max(left,mpp[s[right]]+1);
+            mpp[s[right]]=right;
+            len=max(len,right-left+1);
+            right++;
         }
-        return maxans;
+
+        return len;
     }
 };
