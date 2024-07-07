8c96e413b0aa2b287ad99d525355bfe47fc077b1
harshitgaur077 <harshitgaur737@gmail.com>
2024-07-07 22:58:17 +0530

    6th day

---
+++ day6(linklist)/detectcycle2.cpp
@@ -0,0 +1,39 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+//   Definition for singly-linked list.
+ struct ListNode {
+     int val;
+      ListNode *next;
+      ListNode() : val(0), next(nullptr) {}
+     ListNode(int x) : val(x), next(nullptr) {}
+     ListNode(int x, ListNode *next) : val(x), next(next) {}
+  };
+  class Solution {
+public:
+    ListNode *detectCycle(ListNode *head) {
+                ListNode * fast=head;
+        ListNode * slow=head;
+           bool flag=false;
+        while(fast!=NULL &&fast->next!=NULL){
+            slow=slow->next;
+         
+            fast=fast->next->next;
+            if(slow==fast){
+                flag=true;
+                break;
+            }
+        }
+        if(flag==false)
+        return NULL;
+        else{
+            ListNode * temp=head;
+            while(temp!=slow){
+                slow=slow->next;
+                temp=temp->next;
+            }
+            return slow;
+        }
+        
+    }
+};
