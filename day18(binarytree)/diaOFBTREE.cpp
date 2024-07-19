67fb42d2fc1621229ffeecccd46eaa6519d9afb4
harshitgaur077 <harshitgaur737@gmail.com>
2024-07-20 03:39:13 +0530

    tree

---
+++ day18(binarytree)/diaOFBTREE.cpp
@@ -0,0 +1,36 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+  right(right) {}
+  };
+ 
+class Solution {
+public:
+int levels(TreeNode *root){
+    if(root==NULL)return 0;
+    return 1+max(levels(root->left),levels(root->right));
+    }
+
+void helper(TreeNode *root,int &maxdia){
+     if(root==NULL)return ;
+        int dia=levels(root->left)+levels(root->right);
+        maxdia=max(dia,maxdia);
+        helper(root->left,maxdia);
+        helper(root->right,maxdia);
+        
+}
+
+    int diameterOfBinaryTree(TreeNode* root) {
+        int maxdia=0;
+        helper(root,maxdia);
+        return maxdia;
+    
+    }
+};
