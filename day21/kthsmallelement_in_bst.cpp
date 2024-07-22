111e3fba0502ea8da434887fbe512a39b9a8381c
harshitgaur077 <harshitgaur737@gmail.com>
2024-07-23 03:49:20 +0530

    tree

---
+++ day21/kthsmallelement_in_bst.cpp
@@ -0,0 +1,33 @@
+  #include<bits/stdc++.h>
+  using namespace std;
+
+  struct TreeNode {
+      int val;
+      TreeNode *left;
+      TreeNode *right;
+      TreeNode() : val(0), left(nullptr), right(nullptr) {}
+      TreeNode(int x) : val(x), left(nullptr), right(nullptr) {}
+      TreeNode(int x, TreeNode *left, TreeNode *right) : val(x), left(left),
+  right(right) {}
+  };
+  
+class Solution {
+public:
+    void inorder(TreeNode* root, int& k, int& ans) {
+        if (root == NULL)
+            return;
+        inorder(root->left, k, ans);
+        k--;
+        if (k == 0)
+            ans = root->val;
+
+        inorder(root->right, k, ans);
+    }
+    int kthSmallest(TreeNode* root, int k) {
+        if (root == nullptr)
+            return INT_MIN;
+        int ans = root->val;
+        inorder(root, k, ans);
+        return ans;
+    }
+};
